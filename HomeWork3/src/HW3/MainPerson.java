09054cbebe67e2186c5733d55c114bc3d583e11a
“maria <maria.bzheza@gmail.com>
2016-12-15 17:29:29 +0200

    HomeWork3 added

---
+++ HomeWork3/src/HW3/MainPerson.java
@@ -0,0 +1,18 @@
+package HW3;
+
+public class MainPerson {
+
+	public static void main(String[] args) {
+		Person person1 = new Person("Vasja1", 1980);
+		Person person2 = new Person();
+		Person person3 = new Person();
+		Person person4 = new Person();
+		Person person5 = new Person("Petro", 1995);
+		System.out.println(person1);
+		System.out.println(person2);
+		System.out.println(person3);
+		System.out.println(person4);
+		System.out.println(person5);
+	}
+
+}
