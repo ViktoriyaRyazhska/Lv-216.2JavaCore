54c15efb5f5378d5bb43c85d20878c75de6030d9
“maria <maria.bzheza@gmail.com>
2017-01-03 20:20:48 +0200

    ClassWork8 task 1 and 2 added

---
+++ ClassWork8/src/Task1.java
@@ -0,0 +1,15 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		String str1 = "IT";
+		String str2 = "IT Academy";
+		if(str2.contains(str1)){
+			System.out.println("String2 contains String1");
+		}else {
+			System.out.println("String2 not contains String1");
+		}
+
+	}
+
+}
